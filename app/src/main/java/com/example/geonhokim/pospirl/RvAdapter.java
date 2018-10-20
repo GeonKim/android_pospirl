f15fa5789347b38134c17e405414aa28b48053bd
GeonKim <ghk9204@gmail.com>
2018-10-21 01:21:42 +0900

    feature chart bug fixed

--- app/src/main/java/com/example/geonhokim/pospirl/RvAdapter.java
+++ app/src/main/java/com/example/geonhokim/pospirl/RvAdapter.java
@@ -1,7 +1,9 @@
 package com.example.geonhokim.pospirl;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.Spanned;
@@ -11,7 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -70,9 +72,21 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         String newStr = highlightText(str, position);
 
         itemController.article_btn.setText(datalist.get(position).getTitle());
+        itemController.highlighted_title.setText(datalist.get(position).getTitle());
         itemController.highlighted_texts.setText(str);
 
         itemController.article_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String links = datalist.get(position).getLinks();
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(links));
+                mContext.startActivity(i);
+            }
+        });
+
+        itemController.expandable_btn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
@@ -81,16 +95,13 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
 
                 if ( cnt % 2 == 0 ) //짝수면 차트를 숨기고 아래 화살표
                 {
-                    itemController.expandable_img.setImageResource(R.drawable.ic_expand_more);
+                    itemController.expandable_btn.setImageResource(R.drawable.ic_expand_more);
                     itemController.article_lime_analysis.setVisibility(View.GONE);
                 }else               //홀수면 차트를 보여주고 위 화살표
                 {
-                    itemController.expandable_img.setImageResource(R.drawable.ic_expand_less);
+                    itemController.expandable_btn.setImageResource(R.drawable.ic_expand_less);
                     itemController.article_lime_analysis.setVisibility(View.VISIBLE);
                 }
-//                String links = datalist.get(position).getLinks();
-//                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(links));
-//                mContext.startActivity(i);
             }
         });
     }
@@ -152,6 +163,51 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
     }
 
+    public void plotProbChart(HorizontalBarChart probChart, int position)
+    {
+        float up_prob = (float) datalist.get(position).getUp_prob();
+        float down_prob = (float) datalist.get(position).getDown_prob();
+
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
+        entries2.add(new BarEntry(1, up_prob));
+        entries2.add(new BarEntry(0, down_prob));
+
+
+
+        BarDataSet bardataset2 = new BarDataSet(entries2, "Prob Chart");
+        int[] colorArray = {Color.YELLOW, Color.GRAY};
+        bardataset2.setColors(colorArray);
+        bardataset2.setValueTextSize(12f); //y값 크기
+
+        BarData data2 = new BarData(bardataset2);
+
+        String[] labels2 = {"하락", "상승"};
+        probChart.getXAxis().setValueFormatter(new LabelFormatter(labels2));
+        probChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM); // 그림 왼쪽에 X-value 표시
+        XAxis xAxis = probChart.getXAxis();
+        xAxis.setLabelCount(2);
+
+
+        probChart.getDescription().setEnabled(false); // Hide the description
+        probChart.getAxisLeft().setDrawLabels(false); // 그림의 윗부분 수치(Y-value) 사라짐
+        probChart.getAxisRight().setDrawLabels(false); // 그림 아랫부분 수치(Y-value) 사라짐
+//        probChart.getXAxis().setDrawLabels(false); // 그림 (X-value) 라벨 사라짐
+        probChart.getLegend().setEnabled(false);   // 범례 사라짐
+        probChart.setDrawValueAboveBar(true); // 그림에 중첩되어 value가 그려질 수 있음.
+//        featureChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
+
+        float newLimit = data2.getYMax() + (float) 0.03;
+        probChart.getAxisLeft().setAxisMinimum(0f); // 그래프 표현범위 지정
+        probChart.getAxisLeft().setAxisMaximum(1.1f); // 그래프 표현범위 지정
+        probChart.getAxisLeft().setDrawGridLines(false); // y 왼쪽 그리드 제거
+        probChart.getAxisRight().setDrawGridLines(false); // y 오른쪽 그리드 제거
+        probChart.getXAxis().setDrawGridLines(false); // x그리드 제거
+
+        probChart.animateY(1000);
+        probChart.setData(data2);
+
+    }
+
 
     public void plotFeatureChart(HorizontalBarChart featureChart, int position)
     {
@@ -173,15 +229,14 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         String word5 = datalist.get(position).getWord5();
         String word6 = datalist.get(position).getWord6();
 
-        String[] labels = {word1, word2, word3, word4, word5, word6};
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(6, prob1));
-        entries.add(new BarEntry(5, prob2));
-        entries.add(new BarEntry(4, prob3));
-        entries.add(new BarEntry(3, prob4));
-        entries.add(new BarEntry(2, prob5));
-        entries.add(new BarEntry(1, prob6));
+        entries.add(new BarEntry(5, prob1));
+        entries.add(new BarEntry(4, prob2));
+        entries.add(new BarEntry(3, prob3));
+        entries.add(new BarEntry(2, prob4));
+        entries.add(new BarEntry(1, prob5));
+        entries.add(new BarEntry(0, prob6));
 
 
 
@@ -191,21 +246,25 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         for(int i = 0; i < f_prob.length; i++)
         {
             if(f_prob[i] >= 0){
-                colorArray[i] = Color.BLUE;
+                colorArray[i] = Color.GRAY;
             } else {
                 colorArray[i] = Color.YELLOW;
             }
         }
 
         bardataset.setColors(colorArray);
-        bardataset.setValueTextSize(10f); //y값 크기
+        bardataset.setValueTextSize(12f); //y값 크기
 
 
-        BarData fdata = new BarData(bardataset);
 
-//        featureChart.getXAxis().setValueFormatter(new LabelFormatter(labels));
+        BarData data = new BarData(bardataset);
+
+
+        String[] labels = {word6, word5, word4, word3, word2, word1};
+        featureChart.getXAxis().setValueFormatter(new LabelFormatter(labels));
         featureChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTH_SIDED); // 그림 좌우에 X-value 표시
-
+        XAxis xAxis = featureChart.getXAxis();
+        xAxis.setLabelCount(6);
 
         featureChart.getDescription().setEnabled(false); // Hide the description
         featureChart.getAxisLeft().setDrawLabels(false); // 그림의 윗부분 수치(Y-value) 사라짐
@@ -215,17 +274,17 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         featureChart.setDrawValueAboveBar(true); // 그림에 중첩되어 value가 그려질 수 있음. 그러다가 그래프 밖으로 삐져나오기도 함
 //        featureChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
 
-//        fdata.setValueTextSize(20f);
+//        data.setValueTextSize(20f);
 
-        float newLimit = fdata.getYMax() + (float) 1.03;
-        featureChart.getAxisLeft().setAxisMinimum((float)-0.1); // 그래프 표현범위 지정
-        featureChart.getAxisLeft().setAxisMaximum((float)0.1); // 그래프 표현범위 지정
+        float newLimit = data.getYMax() + (float) 0.03;
+        featureChart.getAxisLeft().setAxisMinimum(-0.1f); // 그래프 표현범위 지정
+        featureChart.getAxisLeft().setAxisMaximum(0.1f); // 그래프 표현범위 지정
         featureChart.getAxisLeft().setDrawGridLines(false); // y 왼쪽 그리드 제거
         featureChart.getAxisRight().setDrawGridLines(false); // y 오른쪽 그리드 제거
         featureChart.getXAxis().setDrawGridLines(false); // x그리드 제거
 
         featureChart.animateY(1000);
-        featureChart.setData(fdata);
+        featureChart.setData(data);
     }
 
     public class LabelFormatter implements IAxisValueFormatter
@@ -242,48 +301,15 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         }
     }
 
-    public void plotProbChart(HorizontalBarChart probChart, int position)
-    {
-        float down_prob = (float) datalist.get(position).getDown_prob();
-        float up_prob = (float) datalist.get(position).getUp_prob();
-        int[] colorArray = {Color.BLUE, Color.YELLOW};
 
-
-
-
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0, down_prob));
-        entries.add(new BarEntry(1, up_prob));
-
-        BarDataSet bardataset = new BarDataSet(entries, "Prob Chart");
-
-        bardataset.setColors(colorArray);
-
-        BarData data = new BarData(bardataset);
-
-
-        probChart.animateY(1000);
-
-        probChart.getDescription().setEnabled(false); // Hide the description
-        probChart.getAxisLeft().setDrawLabels(false);
-        probChart.getAxisRight().setDrawLabels(false);
-        probChart.getXAxis().setDrawLabels(false);
-        probChart.getLegend().setEnabled(false);   // Hide the legend
-
-        //data.setValueTextSize(100f);
-
-        probChart.setData(data);
-
-
-    }
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder
     {
         Button article_btn;
-        ImageView expandable_img;
+        ImageButton expandable_btn;
         LinearLayout article_lime_analysis;
-        TextView highlighted_texts;
+        TextView highlighted_title, highlighted_texts;
         HorizontalBarChart featureChart;
         HorizontalBarChart probChart;
 
@@ -291,8 +317,9 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         {
             super(itemView);
             article_btn = itemView.findViewById(R.id.article_btn);
-            expandable_img = itemView.findViewById(R.id.expandable_img);
+            expandable_btn = itemView.findViewById(R.id.expandable_btn);
             article_lime_analysis = itemView.findViewById(R.id.article_lime_analysis);
+            highlighted_title = itemView.findViewById(R.id.highlight_title);
             highlighted_texts = itemView.findViewById(R.id.highlight_text);
             highlighted_texts.setMovementMethod(new ScrollingMovementMethod());
             featureChart = (HorizontalBarChart) itemView.findViewById(R.id.featureChart);
