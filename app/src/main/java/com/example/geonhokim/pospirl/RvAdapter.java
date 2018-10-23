de211d9f943f13366eceb8abcdfe6257adfb8600
GeonKim <ghk9204@gmail.com>
2018-10-23 17:44:21 +0900

    bug fix

--- app/src/main/java/com/example/geonhokim/pospirl/RvAdapter.java
+++ app/src/main/java/com/example/geonhokim/pospirl/RvAdapter.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
 import android.text.SpannableString;
-import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,22 +63,37 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
     public void onBindViewHolder(final MyViewHolder holder, final int position)
     {
         final RvAdapter.MyViewHolder itemController = (RvAdapter.MyViewHolder) holder;
-        plotFeatureChart(itemController.featureChart, position);
-        plotProbChart(itemController.probChart, position);
-
 
         cnt = 0;
 
+        String word1 = datalist.get(position).getWord1();
+        String word2 = datalist.get(position).getWord2();
+        String word3 = datalist.get(position).getWord3();
+        String word4 = datalist.get(position).getWord4();
+        String word5 = datalist.get(position).getWord5();
+        String word6 = datalist.get(position).getWord6();
+        String[] targetStrings = {word1, word2, word3, word4, word5, word6};
+
+        float prob1 = (float) datalist.get(position).getProb1();
+        float prob2 = (float) datalist.get(position).getProb2();
+        float prob3 = (float) datalist.get(position).getProb3();
+        float prob4 = (float) datalist.get(position).getProb4();
+        float prob5 = (float) datalist.get(position).getProb5();
+        float prob6 = (float) datalist.get(position).getProb6();
+        float[] targetScores = {prob1, prob2, prob3, prob4, prob5, prob6};
+
+        float up_prob = (float) datalist.get(position).getUp_prob();
+        float down_prob = (float) datalist.get(position).getDown_prob();
+        float[] targetProb = {up_prob, down_prob};
+
+        plotFeatureChart(itemController.featureChart, targetScores, targetStrings);
+        plotProbChart(itemController.probChart, targetProb);
+
         String str = datalist.get(position).getContents();
-//        String newStr = highlightText(str, position);
-
-        SpannableString newStr = new SpannableString(str);
-        newStr.setSpan(new BackgroundColorSpan(Color.YELLOW), 0, 11, 0);
-
 
         itemController.article_btn.setText(datalist.get(position).getTitle());
         itemController.highlighted_title.setText(datalist.get(position).getTitle());
-        itemController.highlighted_texts.setText(newStr);
+        itemController.highlighted_texts.setText(getUnderLineColorText(str, targetStrings, targetScores));
 
         itemController.article_btn.setOnClickListener(new View.OnClickListener()
         {
@@ -98,11 +113,11 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
             {
                 cnt += 1;
 
-                if ( cnt % 2 == 0 ) //짝수면 차트를 숨기고 아래 화살표
+                if (cnt % 2 == 0) //짝수면 차트를 숨기고 아래 화살표
                 {
                     itemController.expandable_btn.setImageResource(R.drawable.ic_expand_more);
                     itemController.article_lime_analysis.setVisibility(View.GONE);
-                }else               //홀수면 차트를 보여주고 위 화살표
+                } else               //홀수면 차트를 보여주고 위 화살표
                 {
                     itemController.expandable_btn.setImageResource(R.drawable.ic_expand_less);
                     itemController.article_lime_analysis.setVisibility(View.VISIBLE);
@@ -117,70 +132,32 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         return datalist.size();
     }
 
-    public String highlightText(String inputText, int position)
-    {
-        String word1 = datalist.get(position).getWord1();
-        String word2 = datalist.get(position).getWord2();
-        String word3 = datalist.get(position).getWord3();
-        String word4 = datalist.get(position).getWord4();
-        String word5 = datalist.get(position).getWord5();
-        String word6 = datalist.get(position).getWord6();
+    public static SpannableString getUnderLineColorText(String string, String[] targetStrings, float[] targetScores) {
 
-        String[] words = {word1, word2, word3, word4, word5, word6};
+        SpannableString spannableString = new SpannableString(string);
 
-        float prob1 = (float) datalist.get(position).getProb1();
-        float prob2 = (float) datalist.get(position).getProb2();
-        float prob3 = (float) datalist.get(position).getProb3();
-        float prob4 = (float) datalist.get(position).getProb4();
-        float prob5 = (float) datalist.get(position).getProb5();
-        float prob6 = (float) datalist.get(position).getProb6();
+        for(int i = 0; i < targetStrings.length; i++){
+            int targetStartIndex = string.indexOf(targetStrings[i]);
+            int targetEndIndex = targetStartIndex + targetStrings[i].length();
+            if (targetScores[i] < 0){ //상승 노랑
+                spannableString.setSpan(new BackgroundColorSpan(0x90FCe300), targetStartIndex, targetEndIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-        float[] f_prob = {prob1, prob2, prob3, prob4, prob5, prob6};
-
-
-        String[] token = inputText.split(" ");
-
-        for(int i = 0; i < token.length; i++)
-        {
-            for(int j = 0; j < words.length; j++)
-            {
-                if (token[i] == words[j])
-                {
-                    if (f_prob[j] < 0){
-                        token[i] = "<font color=\"#FFFFB300\">"+token[i]+"</font>";
-                    } else {
-                        token[i] = "<font color=\"#303F9F\">"+token[i]+"</font>";
-                    }
-                }
+            } else {                //하락 파랑
+                spannableString.setSpan(new BackgroundColorSpan(0x905F9EA0), targetStartIndex, targetEndIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             }
         }
-
-        String newStr = TextUtils.join(" ", token);
-
-        return newStr;
+        return spannableString;
     }
 
-//    public static Spanned fromHtml(String source) {
-//        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.N) {
-//            // noinspection deprecation
-//            return Html.fromHtml(source);
-//        }
-//        return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
-//    }
 
-    public void plotProbChart(HorizontalBarChart probChart, int position)
+    public void plotProbChart(HorizontalBarChart probChart, float[] targetProb)
     {
-        float up_prob = (float) datalist.get(position).getUp_prob();
-        float down_prob = (float) datalist.get(position).getDown_prob();
-
         ArrayList<BarEntry> entries2 = new ArrayList<>();
-        entries2.add(new BarEntry(1, up_prob));
-        entries2.add(new BarEntry(0, down_prob));
-
-
+        entries2.add(new BarEntry(1, targetProb[0]));
+        entries2.add(new BarEntry(0, targetProb[1]));
 
         BarDataSet bardataset2 = new BarDataSet(entries2, "Prob Chart");
-        int[] colorArray = {Color.YELLOW, Color.GRAY};
+        int[] colorArray = {Color.YELLOW, Color.BLUE};
         bardataset2.setColors(colorArray);
         bardataset2.setValueTextSize(12f); //y값 크기
 
@@ -190,8 +167,7 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         probChart.getXAxis().setValueFormatter(new LabelFormatter(labels2));
         probChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM); // 그림 왼쪽에 X-value 표시
         XAxis xAxis = probChart.getXAxis();
-        xAxis.setLabelCount(2);
-
+        xAxis.setLabelCount(2); //라벨은 두개만
 
         probChart.getDescription().setEnabled(false); // Hide the description
         probChart.getAxisLeft().setDrawLabels(false); // 그림의 윗부분 수치(Y-value) 사라짐
@@ -199,7 +175,7 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
 //        probChart.getXAxis().setDrawLabels(false); // 그림 (X-value) 라벨 사라짐
         probChart.getLegend().setEnabled(false);   // 범례 사라짐
         probChart.setDrawValueAboveBar(true); // 그림에 중첩되어 value가 그려질 수 있음.
-//        featureChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
+        probChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
 
         //final float newLimit = data2.getYMax() + (float) 0.03;
         probChart.getAxisLeft().setAxisMinimum(-0.05f); // 그래프 표현범위 지정
@@ -208,51 +184,26 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         probChart.getAxisRight().setDrawGridLines(false); // y 오른쪽 그리드 제거
         probChart.getXAxis().setDrawGridLines(false); // x그리드 제거
         probChart.animateY(1000);
-
-
         probChart.setData(data2);
-
     }
 
-
-    public void plotFeatureChart(HorizontalBarChart featureChart, int position)
+    public void plotFeatureChart(HorizontalBarChart featureChart, float[] targetScores, String[] targetStrings)
     {
-
-
-        float prob1 = (float) datalist.get(position).getProb1();
-        float prob2 = (float) datalist.get(position).getProb2();
-        float prob3 = (float) datalist.get(position).getProb3();
-        float prob4 = (float) datalist.get(position).getProb4();
-        float prob5 = (float) datalist.get(position).getProb5();
-        float prob6 = (float) datalist.get(position).getProb6();
-
-        float[] f_prob = {prob1, prob2, prob3, prob4, prob5, prob6};
-
-        String word1 = datalist.get(position).getWord1();
-        String word2 = datalist.get(position).getWord2();
-        String word3 = datalist.get(position).getWord3();
-        String word4 = datalist.get(position).getWord4();
-        String word5 = datalist.get(position).getWord5();
-        String word6 = datalist.get(position).getWord6();
-
-
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(5, prob1));
-        entries.add(new BarEntry(4, prob2));
-        entries.add(new BarEntry(3, prob3));
-        entries.add(new BarEntry(2, prob4));
-        entries.add(new BarEntry(1, prob5));
-        entries.add(new BarEntry(0, prob6));
-
-
+        entries.add(new BarEntry(5, targetScores[0]));
+        entries.add(new BarEntry(4, targetScores[1]));
+        entries.add(new BarEntry(3, targetScores[2]));
+        entries.add(new BarEntry(2, targetScores[3]));
+        entries.add(new BarEntry(1, targetScores[4]));
+        entries.add(new BarEntry(0, targetScores[5]));
 
         BarDataSet bardataset = new BarDataSet(entries, "Feature Chart");
 
-        int[] colorArray = new int[f_prob.length];
-        for(int i = 0; i < f_prob.length; i++)
+        int[] colorArray = new int[targetScores.length];
+        for(int i = 0; i < targetScores.length; i++)
         {
-            if(f_prob[i] >= 0){
-                colorArray[i] = Color.GRAY;
+            if(targetScores[i] >= 0){
+                colorArray[i] = Color.BLUE;
             } else {
                 colorArray[i] = Color.YELLOW;
             }
@@ -264,9 +215,8 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
 
 
         BarData data = new BarData(bardataset);
+        String[] labels = {targetStrings[5], targetStrings[4], targetStrings[3], targetStrings[2], targetStrings[1], targetStrings[0]};
 
-
-        String[] labels = {word6, word5, word4, word3, word2, word1};
         featureChart.getXAxis().setValueFormatter(new LabelFormatter(labels));
         featureChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTH_SIDED); // 그림 좌우에 X-value 표시
         XAxis xAxis = featureChart.getXAxis();
@@ -278,13 +228,11 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
 //        featureChart.getXAxis().setDrawLabels(false); // 그림 (X-value) 라벨 사라짐
         featureChart.getLegend().setEnabled(false);   // 범례 사라짐
         featureChart.setDrawValueAboveBar(true); // 그림에 중첩되어 value가 그려질 수 있음. 그러다가 그래프 밖으로 삐져나오기도 함
-//        featureChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
+        featureChart.setClipValuesToContent(true); // 그래프 밖에 value 삐져나오지 않음.
 
-//        data.setValueTextSize(20f);
-
-        float newLimit = data.getYMax() + (float) 0.03;
-        featureChart.getAxisLeft().setAxisMinimum(-newLimit); // 그래프 표현범위 지정
-        featureChart.getAxisLeft().setAxisMaximum(newLimit); // 그래프 표현범위 지정
+        float newLimit2 = Math.abs(targetScores[0]) * 2;
+        featureChart.getAxisLeft().setAxisMinimum(-newLimit2); // 그래프 표현범위 지정
+        featureChart.getAxisLeft().setAxisMaximum(newLimit2); // 그래프 표현범위 지정
         featureChart.getAxisLeft().setDrawGridLines(false); // y 왼쪽 그리드 제거
         featureChart.getAxisRight().setDrawGridLines(false); // y 오른쪽 그리드 제거
         featureChart.getXAxis().setDrawGridLines(false); // x그리드 제거
@@ -307,9 +255,6 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         }
     }
 
-
-
-
     public static class MyViewHolder extends RecyclerView.ViewHolder
     {
         Button article_btn;
@@ -319,9 +264,6 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
         HorizontalBarChart featureChart;
         HorizontalBarChart probChart;
         ScrollView scrollView;
-
-
-
 
         public MyViewHolder(View itemView)
         {
@@ -333,32 +275,6 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder>
             highlighted_texts = itemView.findViewById(R.id.highlight_text);
             featureChart = (HorizontalBarChart) itemView.findViewById(R.id.featureChart);
             probChart = (HorizontalBarChart) itemView.findViewById(R.id.probChart);
-//            scrollView = (ScrollView) itemView.findViewById(R.id.childScroll);
-
-//            scrollView.setOnTouchListener(new View.OnTouchListener() {
-//
-//                @Override
-//                public boolean onTouch(View v, MotionEvent event) {
-//
-//                    highlighted_texts.getParent().requestDisallowInterceptTouchEvent(false);
-//
-//                    return false;
-//                }
-//            });
-//
-//
-//
-//            highlighted_texts.setOnTouchListener(new View.OnTouchListener()
-//            {
-//                @Override
-//                public boolean onTouch(View view, MotionEvent motionEvent)
-//                {
-//                    highlighted_texts.getParent().requestDisallowInterceptTouchEvent(true);
-//                    return false;
-//                }
-//            });
-//
-//            highlighted_texts.setMovementMethod(new ScrollingMovementMethod());
         }
     }
 
